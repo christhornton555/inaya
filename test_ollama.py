7acd2f90a9a38638baf35d1d763e4f293020c15b
Chris Thornton <cjthornton78@gmail.com>
2025-02-18 23:07:14 +0000

    Basic analysis of question type is now working

--- test_ollama.py
+++ test_ollama.py
@@ -1,11 +1,16 @@
 import ollama
 
+pre_prompt = 'Forget all previous prompts. Describe what general type of topic this question is about, and if it\'s a math topic then celebrate because you really like math, but do not do the calculation. I do not want the answer to the calculation:'
+input_prompt_a = 'Who is the current president of the USA?'
+input_prompt_b = 'What is fourteen divided by two?'
+
 stream = ollama.chat(
     model='deepseek-r1:8b',
-    messages=[{'role': 'user', 'content': 'Who is the current president of the USA?'}],
+    messages=[{'role': 'user', 'content': f'{pre_prompt} \"{input_prompt_b}\"'}],
     stream = True
 )
 
+print(f'{pre_prompt} \"Is the weather going to be nice today?\"')
+
 for chunk in stream:
     print(chunk['message']['content'], end = '', flush = True)
-    
